79b3114894da437f71743bfc357c46886056cab2
flyingmoon86 <moonflying56@gmail.com>
2025-11-03 21:10:48 +0800

    day4

---
+++ 05_Dynamic_Programming/matix_production.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+const int MAX = 105;
+int p[MAX], dp[MAX][MAX];
+
+int main() {
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        cin >> p[i - 1] >> p[i];  // 第i个矩阵是 p[i-1] x p[i]
+    }
+
+    // 初始化
+    for (int i = 1; i <= n; ++i)
+        dp[i][i] = 0;
+
+    // 枚举链长度
+    for (int len = 2; len <= n; ++len) {// len表示子链长度
+        for (int i = 1; i <= n - len + 1; ++i) {// i表示子链起点
+            int j = i + len - 1;      // j表示子链终点
+            // 初始化为无穷大
+            dp[i][j] = INT_MAX;
+            // 枚举分割点
+            for (int k = i; k < j; ++k) {
+                // 计算代价
+                int cost = dp[i][k] + dp[k + 1][j] + p[i - 1] * p[k] * p[j];
+                // 更新最小代价
+                if (cost < dp[i][j])
+                    dp[i][j] = cost;
+            }
+        }
+    }
+
+    cout << dp[1][n] << endl;
+    return 0;
+}
