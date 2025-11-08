5a6add9104a47b8d5859526d6a4e919c9162caa0
flyingmoon86 <moonflying56@gmail.com>
2025-11-08 16:30:27 +0800

    matixpro

--- 05_Dynamic_Programming/matix_production.cpp
+++ 05_Dynamic_Programming/matix_production.cpp
@@ -17,13 +17,13 @@ int main() {
         dp[i][i] = 0;
 
     // 枚举链长度
-    for (int len = 2; len <= n; ++len) {// len表示子链长度
-        for (int i = 1; i <= n - len + 1; ++i) {// i表示子链起点
-            int j = i + len - 1;      // j表示子链终点
+    for (int len = 2; len <= n; ++len) {// len表示子链长度，第一层循环控制规模
+        for (int i = 1; i <= n - len + 1; ++i) {// i表示子链起点，第二次循环控制起点
+            int j = i + len - 1;      // j表示子链终点，根据起点和长度计算终点
             // 初始化为无穷大
             dp[i][j] = INT_MAX;
             // 枚举分割点
-            for (int k = i; k < j; ++k) {
+            for (int k = i; k < j; ++k) {// k表示分割点，第三层循环控制分割点
                 // 计算代价
                 int cost = dp[i][k] + dp[k + 1][j] + p[i - 1] * p[k] * p[j];
                 // 更新最小代价
